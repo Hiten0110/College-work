0be431d6bc187fb134b27207b5250d843bd1faae
Hiten <hitenmahendru03@gmail.com>
2026-07-15 18:36:11 +0530

    Hiten5

---
+++ src/pages/HR/Dash.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import WeeklyChart from "./Weeklychart";
+
+
+function Dash() {
+
+
+  return (
+    <div className="p-6">
+        <WeeklyChart/>
+ 
+
+
+    </div>
+  );
+}
+
+export default Dash;
